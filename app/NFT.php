37d3b20a7132a2f5303b1fe063b2e72de18ac852
Adnan Siddiqi <kadnan@gmail.com>
2021-09-09 16:17:23 +0500

    NFT model file

--- app/NFT.php
+++ app/NFT.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class NFT extends Model
-{
-    //
+class NFT extends Model {
+	protected $table = 'nfts';
+	protected $fillable = [ 'car_id', 'meta' ];
 }
